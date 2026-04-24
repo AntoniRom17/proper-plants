889d26cfae74ec8da1e2ff26181edbbce7a615db
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-23 20:07:51 -0500

    add increment and decrement buttons

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -4,6 +4,9 @@ export default function CartItem({ item, increment, decrement }) {
       <span>{item.image}</span>
       <h3>{item.name}</h3>
       <p>Quantity: {item.quantity}</p>
+
+      <button onClick={() => decrement(item.id)}>-</button>
+      <button onClick={() => increment(item.id)}>+</button>
     </div>
   );
 }
