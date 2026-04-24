a15021900f329cd02ab00f590e8d949f577b0653
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-23 20:05:07 -0500

    add grid container to plants component

--- src/components/Plants/Plants.jsx
+++ src/components/Plants/Plants.jsx
@@ -2,6 +2,10 @@ export default function Plants({ plants, cart, addToCart }) {
   return (
     <div>
       <h2>Available Plants</h2>
+
+      <div className="plants-grid">
+        {/* PlantCard components will go here */}
+      </div>
     </div>
   );
 }
