5dc0bbf317c21aab3606e176d3f0da09bff20acf
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-23 20:03:21 -0500

    add already in cart and button label

--- src/components/Plants/PlantCard.jsx
+++ src/components/Plants/PlantCard.jsx
@@ -1,9 +1,13 @@
 export default function PlantCard({ plant, cart, addToCart }) {
+  const inCart = cart.find((item) => item.id === plant.id);
+
   return (
     <div>
       <span>{plant.image}</span>
       <h3>{plant.name}</h3>
-      <button onClick={() => addToCart(plant)}>Add to cart</button>
+      <button onClick={() => addToCart(plant)}>
+        {inCart ? "Add another" : "Add to cart"}
+      </button>
     </div>
   );
 }
