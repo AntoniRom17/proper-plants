1b4577a085e3e61e1d7f5b56a2fb44cae23de058
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-23 20:05:39 -0500

    render PlantCard list inside Plants component

--- src/components/Plants/Plants.jsx
+++ src/components/Plants/Plants.jsx
@@ -1,10 +1,19 @@
+import PlantCard from "./PlantCard";
+
 export default function Plants({ plants, cart, addToCart }) {
   return (
     <div>
       <h2>Available Plants</h2>
 
       <div className="plants-grid">
-        {/* PlantCard components will go here */}
+        {plants.map((plant) => (
+          <PlantCard
+            key={plant.id}
+            plant={plant}
+            cart={cart}
+            addToCart={addToCart}
+          />
+        ))}
       </div>
     </div>
   );
