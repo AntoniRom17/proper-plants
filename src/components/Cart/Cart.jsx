b71945c1dc9ac0715626c0d24ec6dc6757cb1c9e
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-23 20:09:31 -0500

    add cart items container to cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,10 @@ export default function Cart({ cart, increment, decrement }) {
   return (
     <div>
       <h2>Your Cart</h2>
+
+      <div className="cart-items">
+        {/* CartItem components will go here */}
+      </div>
     </div>
   );
 }
