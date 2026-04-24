39556cef62c6e76b1d70714a504c925888429e9c
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-23 20:10:05 -0500

    render cartitem list inside cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,19 @@
+import CartItem from "./CartItem";
+
 export default function Cart({ cart, increment, decrement }) {
   return (
     <div>
       <h2>Your Cart</h2>
 
       <div className="cart-items">
-        {/* CartItem components will go here */}
+        {cart.map((item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            increment={increment}
+            decrement={decrement}
+          />
+        ))}
       </div>
     </div>
   );
